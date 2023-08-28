05c0303d24e62e67aadfc1f134b7d75184fa9a02
Lawerh Caesar <lawerhcaesar@gmail.com>
2023-08-28 14:01:28 +0000

    added post method to form

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $subject = $_POST["subject"];
 $message = $_POST["message"];
 
 
-$EmailTo = "joshua@dexteradei.com";
+$EmailTo = "lawerhcaesar@gmail.com";
 $Title = "New Message Received";
 
 // prepare email body text
