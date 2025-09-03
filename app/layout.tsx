99e8e85eeff04ac370d47502e0bfdd9860c64d20
Gideon Fiawornu <gfiawornu@persol.com>
2025-09-03 09:13:34 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,40 +10,38 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
-  title: "Katachi — Design furniture for spaces that breathe.",
-  description: "Architected in Belgium, built to last—timeless pieces.",
-  generator: "v0.app",
-  alternates: {
-    canonical: "https://katachi.example/",
-  },
+  title: "Dextera Dei .",
+  description: "Crafting Timeless Spaces, Shaping the Future.",
+  generator: "virtuoso",
+
   openGraph: {
-    siteName: "Katachi",
-    title: "Design furniture for spaces that breathe. | Katachi",
-    description: "Architected in Belgium, built to last—timeless pieces.",
+    siteName: "Dextera Dei",
+    title: "Crafting Timeless Spaces, Shaping the Future | Dextera Dei",
+    description: "Crafting Timeless Spaces, Shaping the Future.",
     type: "website",
-    url: "https://katachi.example/",
-    images: [
-      {
-        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/opengraph-katachi.jpg-7vz2r3hxZA6woukGOmH115Fg7Piyjs.jpeg",
-        alt: "Katachi design furniture — timeless pieces, architected in Belgium",
-        width: 1200,
-        height: 630,
-      },
-    ],
-    locale: "en_BE",
+    // url: "https://katachi.example/",
+    // images: [
+    //   {
+    //     url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/opengraph-katachi.jpg-7vz2r3hxZA6woukGOmH115Fg7Piyjs.jpeg",
+    //     alt: "Katachi design furniture — timeless pieces, architected in Belgium",
+    //     width: 1200,
+    //     height: 630,
+    //   },
+    // ],
+    locale: "en_US",
   },
-  twitter: {
-    card: "summary_large_image",
-    title: "Design furniture for spaces that breathe. | Katachi",
-    description: "Architected in Belgium, built to last—timeless pieces.",
-    images: [
-      {
-        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/opengraph-katachi.jpg-7vz2r3hxZA6woukGOmH115Fg7Piyjs.jpeg",
-        alt: "Katachi design furniture — timeless pieces, architected in Belgium",
-      },
-    ],
-    site: "@katachi",
-  },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: "Design furniture for spaces that breathe. | Katachi",
+  //   description: "Architected in Belgium, built to last—timeless pieces.",
+  //   images: [
+  //     {
+  //       url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/opengraph-katachi.jpg-7vz2r3hxZA6woukGOmH115Fg7Piyjs.jpeg",
+  //       alt: "Katachi design furniture — timeless pieces, architected in Belgium",
+  //     },
+  //   ],
+  //   site: "@katachi",
+  // },
 }
 
 export default function RootLayout({
