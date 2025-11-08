0166b1fd51d21e2c2d7c0e46f1ee9b152fa0b910
herocodes770 <herocodes770@gmail.com>
2025-11-08 09:57:41 +0000

    Add logo for favicon and Open Graph (Google searches and social sharing)

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,35 +15,37 @@ export const metadata: Metadata = {
   title: "Dextera Dei .",
   description: "Crafting Timeless Spaces, Shaping the Future.",
   generator: "virtuoso",
+  icons: {
+    icon: "/Dex Lite.png",
+    apple: "/Dex Lite.png",
+  },
 
   openGraph: {
     siteName: "Dextera Dei",
     title: "Crafting Timeless Spaces, Shaping the Future | Dextera Dei",
     description: "Crafting Timeless Spaces, Shaping the Future.",
     type: "website",
-    // url: "https://katachi.example/",
-    // images: [
-    //   {
-    //     url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/opengraph-katachi.jpg-7vz2r3hxZA6woukGOmH115Fg7Piyjs.jpeg",
-    //     alt: "Katachi design furniture — timeless pieces, architected in Belgium",
-    //     width: 1200,
-    //     height: 630,
-    //   },
-    // ],
+    images: [
+      {
+        url: "/Dex Lite.png",
+        alt: "Dextera Dei - Architectural Artistry and Craftsmanship",
+        width: 1200,
+        height: 630,
+      },
+    ],
     locale: "en_US",
   },
-  // twitter: {
-  //   card: "summary_large_image",
-  //   title: "Design furniture for spaces that breathe. | Katachi",
-  //   description: "Architected in Belgium, built to last—timeless pieces.",
-  //   images: [
-  //     {
-  //       url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/opengraph-katachi.jpg-7vz2r3hxZA6woukGOmH115Fg7Piyjs.jpeg",
-  //       alt: "Katachi design furniture — timeless pieces, architected in Belgium",
-  //     },
-  //   ],
-  //   site: "@katachi",
-  // },
+  twitter: {
+    card: "summary_large_image",
+    title: "Crafting Timeless Spaces, Shaping the Future | Dextera Dei",
+    description: "Crafting Timeless Spaces, Shaping the Future.",
+    images: [
+      {
+        url: "/Dex Lite.png",
+        alt: "Dextera Dei - Architectural Artistry and Craftsmanship",
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
